1144c8d0683987a8b3cfd022e1725326dbc5db0b
Dmitry Khamitsevich <bullet.xenus@gmail.com>
2020-09-09 22:55:40 +0300

    feat: fix function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,71 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let arr = expr.split('').filter(val => val !== ' ');
+
+    for (let i = 0; i < arr.length; i++) {
+        if ((!isNaN(arr[i])) && (!isNaN(arr[i + 1]))) {
+            arr[i] = arr[i] + arr[i + 1];
+            arr.splice(i + 1, 1);
+            i--;
+        }
+    }
+    arr = arr.map(val => isNaN(val) ? val : +val);
+
+    const leftBracket = arr.filter(val => val === '(').length;
+    const rightBracket = arr.filter(val => val === ')').length;
+    if (leftBracket !== rightBracket) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    };
+
+    arr.forEach((val, index, arr) => {
+        if ((val === '/') && arr[index + 1] === 0) {
+            throw new Error("TypeError: Division by zero.");
+        }
+    })
+
+    function calc(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === '*') {
+                arr[i - 1] = arr[i - 1] * arr[i + 1];
+                arr.splice(i, 2);
+                i--;
+            } else if (arr[i] === '/') {
+                arr[i - 1] = arr[i - 1] / arr[i + 1];
+                arr.splice(i, 2);
+                i--;
+            }
+        }
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === '+') {
+                arr[i - 1] = arr[i - 1] + arr[i + 1];
+                arr.splice(i, 2);
+                i--;
+            } else if (arr[i] === '-') {
+                arr[i - 1] = arr[i - 1] - arr[i + 1];
+                arr.splice(i, 2);
+                i--;
+            }
+        }
+        return arr[0];
+    }
+
+    function brackets(arr) {
+        const finish = arr.indexOf(')');
+        for (let i = finish; i >= 0; i--) {
+            if (arr[i] === '(') {
+                let newArr = arr.splice(i, finish - i + 1);
+                newArr = newArr.slice(1, -1);
+                let result = calc(newArr);
+                arr.splice(i, 0, result);
+                if (arr.indexOf(')') !== -1) {
+                    brackets(arr);
+                }
+            }
+        }
+    }
+    brackets(arr);
+    return calc(arr);
 }
 
 module.exports = {
